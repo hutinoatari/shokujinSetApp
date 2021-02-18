2795ae6dc28be31a43b2abf2fa82f4ab6e7b8540
hutinoatari <hutinoatari@gmail.com>
2021-02-19 02:27:17 +0900

    各ページとナビゲーションを追加した

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,23 @@
 import { FC } from "react";
+import Link from "next/link";
 
 const Header: FC<{}> = () => {
     return (
         <header>
             <h1>食神セタップ</h1>
+            <nav>
+                <Link href="/" replace>
+                    <a>TOP</a>
+                </Link>
+                ・
+                <Link href="/menu" replace>
+                    <a>メニュー</a>
+                </Link>
+                ・
+                <Link href="/order" replace>
+                    <a>注文</a>
+                </Link>
+            </nav>
         </header>
     );
 };
