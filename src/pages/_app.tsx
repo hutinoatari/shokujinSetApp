d61dbec02aa7264fec2e5202174119158906fd0a
hutinoatari <hutinoatari@gmail.com>
2021-02-19 01:17:04 +0900

    ヘッダーとフッターを追加した。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
 import "../styles/App.scss";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Header />
+            <main>
+                <Component {...pageProps} />
+            </main>
+            <Footer />
+        </>
+    );
 };
 
 export default App;
