557af65f598eace13074006c6f6b1225d776967b
hutinoatari <hutinoatari@gmail.com>
2021-04-17 14:26:59 +0900

    メニューと机のデータ作った

---
+++ src/datas/tableData.tsx
@@ -0,0 +1,61 @@
+export interface tableDataType {
+    name: string;
+    orderName: string;
+}
+
+const tableDatas: tableDataType[] = [
+    {
+        name: "円卓A",
+        orderName: "テーブルA",
+    },
+    {
+        name: "円卓B",
+        orderName: "テーブルB",
+    },
+    {
+        name: "円卓C",
+        orderName: "テーブルC",
+    },
+    {
+        name: "テーブル一",
+        orderName: "一番テーブル",
+    },
+    {
+        name: "テーブル二",
+        orderName: "二番テーブル",
+    },
+    {
+        name: "テーブル三",
+        orderName: "三番テーブル",
+    },
+    {
+        name: "テーブル四",
+        orderName: "四番テーブル",
+    },
+    {
+        name: "テーブル五",
+        orderName: "五番テーブル",
+    },
+    {
+        name: "テーブル六",
+        orderName: "六番テーブル",
+    },
+    {
+        name: "テーブル七",
+        orderName: "七番テーブル",
+    },
+    {
+        name: "テーブル八",
+        orderName: "八番テーブル",
+    },
+    {
+        name: "テーブル九",
+        orderName: "九番テーブル",
+    },
+    {
+        name: "テーブル十",
+        orderName: "十番テーブル",
+    },
+];
+
+export { tableDatas };
