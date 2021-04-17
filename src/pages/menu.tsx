67afbf0f1df48bb48bdd6f84e1959cc1e5c2fdce
hutinoatari <hutinoatari@gmail.com>
2021-04-17 15:17:43 +0900

    メニューページ書いた

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,10 +1,25 @@
 import { FC } from "react";
+import { menuDataType, menuDatas, dayMenuDatas } from "../datas/menuData";
 
 const Menu: FC<{}> = () => {
     return (
         <>
-            <h2>メニュー</h2>
-            <p>簡易メニュー表のページ。</p>
+            <h2>定食メニュー</h2>
+            <ul>
+                {menuDatas.map((menuData: menuDataType, i: number) => (
+                    <li key={`set-${i}`}>{menuData.name}</li>
+                ))}
+            </ul>
+
+            <h2>曜替わり定食メニュー</h2>
+            <p>日曜メニューは土曜日に注文可能です。</p>
+            <ul>
+                {dayMenuDatas.map((menus: menuDataType[], i: number) =>
+                    menus.map((menu: menuDataType, j: number) => (
+                        <li key={`daySet-${i}-${j}`}>{menu.name}</li>
+                    ))
+                )}
+            </ul>
         </>
     );
 };
