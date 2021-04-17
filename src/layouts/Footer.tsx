33f0024407e70383619d8fbb1141e3f64aada72e
hutinoatari <hutinoatari@gmail.com>
2021-04-17 13:30:47 +0900

    トップページ書いた

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Footer: FC<{}> = () => {
     return (
-        <footer style={{textAlign: "center", backgroundColor: "burlywood"}}>
+        <footer style={{ textAlign: "center", backgroundColor: "burlywood" }}>
             <p>
                 <small>(C)2021 淵野アタリ</small>
             </p>
