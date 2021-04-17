871bed57fd08ec92c656244ca8fa1fe30d5930da
hutinoatari <hutinoatari@gmail.com>
2021-04-17 17:33:54 +0900

    注文できるようにした

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { tableDataType, tableDatas } from "../datas/tableData";
 import {
     menuDataType,
@@ -8,13 +8,20 @@ import {
 } from "../datas/menuData";
 
 const Order: FC<{}> = () => {
+    const [order, setOrder] = useState<string>("");
+    const [orders, setOrders] = useState<string[]>([]);
+    const [tableNum, setTableNum] = useState<string>("");
+    
     return (
         <>
             <h2>選択</h2>
             <div>
                 <label>
                     メニュー:
-                    <select>
+                    <select
+                        value={order}
+                        onChange={(e) => setOrder(e.target.value)}
+                    >
                         <option value="">未選択</option>
                         <optgroup label="定食">
                             {menuDatas.map(
@@ -57,13 +64,26 @@ const Order: FC<{}> = () => {
             </div>
 
             <div>
-                <button>追加</button>
+                <button
+                    onClick={(e) => {
+                        if (order === "") return;
+                        const newOrders = [...orders];
+                        newOrders.push(order);
+                        setOrder("");
+                        setOrders(newOrders);
+                    }}
+                >
+                    追加
+                </button>
             </div>
 
             <h2>注文リスト</h2>
             <label>
                 机:
-                <select>
+                <select
+                    value={tableNum}
+                    onChange={(e) => setTableNum(e.target.value)}
+                >
                     <option value="">未選択</option>
                     {tableDatas.map((tableData: tableDataType, i: number) => (
                         <option key={`table-${i}`} value={tableData.orderName}>
@@ -72,10 +92,38 @@ const Order: FC<{}> = () => {
                     ))}
                 </select>
             </label>
-            <ol></ol>
+            <ol>
+                {orders.map((e, i) => (
+                    <li key={`order-${i}`}>{e}</li>
+                ))}
+            </ol>
 
             <h2>送信</h2>
-            <button>LINEする</button>
+            <button
+                onClick={(e) => {
+                    if (tableNum === "") {
+                        alert("テーブル番号を入力して下さい。");
+                        return;
+                    }
+                    if (orders.length === 0) {
+                        alert("メニューを入力して下さい。");
+                        return;
+                    }
+                    if (
+                        !window.confirm(
+                            '注文を決定しますか？\n("はい"を押すとLINEを開きます。)'
+                        )
+                    )
+                        return;
+                    const orderText = `${tableNum}\n${orders.join("\n")}`;
+                    const url = `https://line.me/R/msg/text/?${encodeURI(
+                        orderText
+                    )}`;
+                    window.open(url, "_blank");
+                }}
+            >
+                LINEする
+            </button>
         </>
     );
 };
